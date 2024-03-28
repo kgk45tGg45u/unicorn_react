769e1d5c183b3cc897a94e51d48c077d21086711
Bijan Sabbagh <bijans@gmail.com>
2024-03-28 22:13:24 +0100

    home

--- src/components/HomeCard.js
+++ src/components/HomeCard.js
@@ -1,8 +1,6 @@
-// import {useState } from 'react'
 import { Link } from "react-router-dom"
-// import BasicModal from "./Popover"
 
-export const DashboardCard = (props) => {
+export const HomeCard = (props) => {
   // const [hidden, setHidden] = useState(true);
 
   return (
@@ -11,18 +9,16 @@ export const DashboardCard = (props) => {
         <Link to={props.link}>
           <div
             onClick={props.onClick}
-            className="dashboard_icon"
           >
-            <img src={props.icon} alt="Tokens" />
+            <img src={props.img} alt={props.alt} />
             {/* <span className='indicator'>22</span> */}
             <p>{props.text}</p>
           </div>
         </Link>
       ) : (<div
         onClick={props.onClick}
-        className="dashboard_icon"
       >
-        <img src={props.icon} alt="Tokens" />
+        <img src={props.img} alt="Tokens" />
         {/* <span className='indicator'>22</span> */}
         <p>{props.text}</p>
       </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,10 @@ import '../assets/home.css'
 import { Link } from "react-router-dom";
 import { useFetch } from '../hooks/useFetch'
 import { useEffect } from 'react'
+import { HomeCard } from '../components/HomeCard'
+import handshake from '../assets/SVG/handshake-solid.svg'
+import shop from '../assets/SVG/shop-solid.svg'
+import users from '../assets/SVG/users-gear-solid.svg'
 
 export const Home = () => {
   const id = null
@@ -32,38 +36,20 @@ export const Home = () => {
         <div className="banner divbg p-4" style={{
           backgroundImage: `url(${process.env.PUBLIC_URL + '/img/Sahara_desert_sunrise.jpg'})`
         }}>
-          <h1 className="p-4 text-white">Creating a planned <strong>Socialist Economy</strong> for <strong>everyone</strong>!</h1>
-          <p className="p-4">Modelling the future of labor, market and planning with the help of smart designs.</p>
+          <h1>Creating a planned <strong>Socialist Economy</strong> for <strong>everyone</strong>!</h1>
+          <p>Modelling the future of labor, market and planning with the help of smart designs.</p>
           <Link to="/login" className="mx-4 btn btn-danger">Join now</Link>
         </div>
 
         <div className="d-flex m-4 align-content-start justify-content-evenly flex-wrap">
-          <div className="col-md-4 divbg col-sm-12 homeCard p-3 border m-4 shadow">
-            <h3 className="h3">Unicorn 0.1</h3>
-            <ul>
-              <li>currently serving {noOfUsers.length} users</li>
-              <li>integrating work and politics in an online App</li>
-              <li>Access to all the data for everyone with the help of block-chain.</li>
-            </ul>
-            {/* <button className="btn btn-danger">Logout</button> */}
+          <div className="col-md-3 divbg col-sm-12 homeCard p-3 border m-4 shadow">
+            <HomeCard img={shop} text="Unicorn's pseudo-market makes transactions easier and payments safer." alt="Pseudo-market" />
           </div>
-          <div className="col-md-4 divbg col-sm-12 homeCard p-3 border m-4 shadow">
-            <h3 className="h3">Unicorn 0.1</h3>
-            <ul>
-              <li>currently serving 1549 users</li>
-              <li>integrating work and politics in an online App</li>
-              <li>Access to all the data for everyone with the help of block-chain.</li>
-            </ul>
-            {/* <button className="btn btn-danger">Logout</button> */}
+          <div className="col-md-3 divbg col-sm-12 homeCard p-3 border m-4 shadow">
+            <HomeCard img={handshake} text="Smart contracts makes it easier for everyone to start their business online." alt="Free contracts" />
           </div>
-          <div className="col-md-4 divbg col-sm-12 homeCard p-3 border m-4 shadow">
-            <h3 className="h3">Unicorn 0.1</h3>
-            <ul>
-              <li>currently serving 1444 users</li>
-              <li>integrating work and politics in an online App</li>
-              <li>Access to all the data for everyone with the help of block-chain.</li>
-            </ul>
-            {/* <button className="btn btn-danger">Logout</button> */}
+          <div className="col-md-3 divbg col-sm-12 homeCard p-3 border m-4 shadow">
+          <HomeCard img={users} text="Socialism means you shop with lower prices and work for yourself and your community." alt="Socialism" />
           </div>
         </div>
       </div>
