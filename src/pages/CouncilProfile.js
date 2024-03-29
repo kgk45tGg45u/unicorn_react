a29dab424c698e3b4ccd46d28c7d8a6923070571
Bijan Sabbagh <bijans@gmail.com>
2024-03-29 16:54:11 +0100

    removed unused imports

--- src/pages/CouncilProfile.js
+++ src/pages/CouncilProfile.js
@@ -8,12 +8,6 @@ import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import List from '@mui/material/List';
 import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 
 import profilePlaceholder from '../assets/profile-image-placeholder.jpeg'
 import moneyBill from '../assets/SVG/money-bill-solid.svg'
